cb05a1c1a5ba14875b191e082eaeae4cf4ef222a
Yuheng <799810767@qq.com>
2025-11-06 09:56:29 -0600

    Initial Commit

---
+++ RTMDetSwift/RTMDetSwift.swift
@@ -0,0 +1,9 @@
+//
+//  RTMDetSwift.swift
+//  RTMDetSwift
+//
+//  Created by 吴宇恒 on 11/6/25.
+//
+
+import Foundation
+
---
+++ RTMDetSwiftTests/RTMDetSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  RTMDetSwiftTests.swift
+//  RTMDetSwiftTests
+//
+//  Created by 吴宇恒 on 11/6/25.
+//
+
+import Testing
+@testable import RTMDetSwift
+
+struct RTMDetSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
